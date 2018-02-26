0b13b3190ab7a0616cc55e0545eb67b4162c3163
hdorin <hdorin@users.noreply.github.com>
2018-02-26 22:35:06 +0200

    V0.3

--- Lab2/src/lab2/Person.java
+++ Lab2/src/lab2/Person.java
@@ -1,10 +1,39 @@
 package lab2;
 
-public class Person {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public abstract class Person {
+	private String name,email,sex;
+	private int age;
+	public Person(String name,String email,String sex,int age) {
+		this.name=name;
+		this.email=email;
+		this.sex=sex;
+		this.age=age;
 	}
+	public String getName() {
+		return name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public void setEmail(String email) {
+		this.email=email;
+	}
+	public void setSex(String sex) {
+		this.sex=sex;
+	}
+	public void setAge(int age) {
+		this.age=age;
+	}
+	
+
 
 }
