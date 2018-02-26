4bd4d9b2e312110718e1985add4d0405c0210821
Dell <slayers_mihai@yahoo.com>
2018-02-26 22:35:56 +0200

    v0.4

--- Lab2/src/lab2/Student.java
+++ Lab2/src/lab2/Student.java
@@ -1,6 +1,23 @@
 package lab2;
 
-public class Student {
-    private String fname;
-    private String lnmae;
+import java.util.Vector;
+
+public class Student extends Person {
+    private Vector prefList;
+    private Integer nrOfPref;
+    public Student(){
+
+    }
+
+    public Student(Vector prefList, Integer nrOfPref){
+        this.nrOfPref = nrOfPref;
+        this.prefList = prefList;
+    }
+
+    public void printPrefList(){
+        for (int i = 0;i < nrOfPref; i++)
+            System.out.println(prefList.get(i));
+    }
+
+
 }
