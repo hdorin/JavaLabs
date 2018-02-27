2f7d172094145abc84c3f3fc8d037c0194e77556
Dorin Haloca <halocadorin@outlook.com>
2018-02-27 18:53:41 +0200

    V0.64

--- Lab2/src/lab2/Person.java
+++ Lab2/src/lab2/Person.java
@@ -1,19 +1,34 @@
 package lab2;
-
-class Person {
-
+/**
+ * Is an abstract class which is derived in Stundent.java
+ * @param teachers the list of students involved in the project
+ */
+abstract class Person {
+	
 	private String name, email;
 
-
+	/**
+	 * Class constructor 
+	 * @param name
+	 * @param email
+	 */
 	public Person(String name, String email) {
 		this.name = name;
 		this.email = email;
 	}
-
+	
+	/**
+	 * Getter for the variable name of type String
+	 * @return name the name of the person 
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Getter for the variable email of type String
+	 * @return email
+	 */
 	public String getEmail() {
 		return email;
 	}
--- Lab2/src/lab2/Problem.java
+++ Lab2/src/lab2/Problem.java
@@ -2,31 +2,51 @@ package lab2;
 
 import java.util.Vector;
 
+/**
+ * This class stores the the participants involved, be them
+ * students or teachers. 
+ */
+
 class Problem {
 	private Vector<Student> students = new Vector<Student>(20);
 	private Vector<Teacher> teachers = new Vector<Teacher>(20);
 
+	/**
+	 * Sets the students who are involved 
+	 * @param students the list of students involved
+	 */
 	public void setStudents(Student ... students) {
         for (Student student: students) {
             this.students.add(student);
         }
 	}
-
+	
+	/**
+	 * Sets the teachers involved
+	 * @param teachers the list of students involved
+	 */
 	public void setTeachers(Teacher ... teachers) {
 		for (Teacher teacher: teachers) {
             this.teachers.add(teacher);
         }
 	}
-
+	
+	/**
+	 * Returns a list of participants 
+	 * @return participants in a vector of type Person
+	 */
 	public Vector<Person> getParticipants() {
 		return null;
 		
 	}
 
 	@Override
+	/**
+	 * Returns a string with the elements of the object 
+	 * @return string with the students and teachers involved
+	 */
 	public String toString() {
 		// TODO Auto-generated method stub
 		return super.toString() + this.students + this.teachers;
 	}
-
 }
