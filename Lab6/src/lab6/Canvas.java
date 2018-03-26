d9ff5ada3792b44fd0e0b74392bc20ccff56d908
Dell <slayers_mihai@yahoo.com>
2018-03-26 22:37:05 +0300

    0.2

--- Lab6/src/lab6/Canvas.java
+++ Lab6/src/lab6/Canvas.java
@@ -2,26 +2,66 @@ package lab6;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Random;
+import java.awt.event.MouseListener;
 
-public class Canvas extends JPanel {
+public class Canvas extends JPanel implements MouseListener {
 
-    private Graphics2D graphics;
-    private Integer radius;
-    private Integer sides;
+//    private Graphics2D graphics;
+//
+//    public void drawShapeAt(int x, int y) {
+//        super.paintComponent(graphics);
+//        Random rand = new Random();
+//        graphics.setColor(new Color(rand.nextInt(0xFFFFFF)));
+////          ...
+//        Integer radius = 5;
+//        Integer sides = 4;
+//        graphics.fill(new RegularPolygon(x, y, radius, sides));
+//
+//        this.addMouseListener(new MouseAdapter() {
+//            public void mouseClicked(MouseEvent e) {
+//                drawShapeAt(e.getX(), e.getY());
+//            }
+//        });
+//
+//    }
 
-    public void drawShapeAt(int x, int y) {
-        Random rand = new Random();
-        graphics.setColor(new Color(rand.nextInt(0xFFFFFF)));
-//		...
-        graphics.fill(new RegularPolygon(x, y, radius, sides));
+    Canvas() {
+//        this.graphics = (Graphics2D) getGraphics();
+        addMouseListener(this);
+        this.setSize(300,300);
+        this.setLayout(null);
+        this.setVisible(true);
 
-        this.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                drawShapeAt(e.getX(), e.getY());
-            }
-        });
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Graphics2D g = (Graphics2D) getGraphics();
+        g.setColor(Color.BLUE);
+//        g.fillOval(e.getX(), e.getY(), 40, 40);
+        g.fillRect(e.getX(), e.getY(), 10,40);
+    }
+
+
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
--- Lab6/src/lab6/Main.java
+++ Lab6/src/lab6/Main.java
@@ -1,8 +1,22 @@
 package lab6;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+//        JFrame frame = new JFrame("Test");
+//        frame.setVisible(true);
+//        frame.setSize(400,200);
+//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        Canvas object = new Canvas();
+//        frame.add(object);
+
+//        new Canvas();
+        JFrame frame = new JFrame("Tst");
+        frame.setVisible(true);
+        frame.setSize(400,400);
+        frame.add(new Canvas());
     }
 }
